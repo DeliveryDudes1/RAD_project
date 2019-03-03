bd1778b01791b693ac2dc6b41f8caeff46448d57
ArifMatin <arif.matin@mail.itsligo.ie>
2019-03-03 19:54:16 +0000

    set up

---
+++ RADProjectWebAPI/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDomain.Classes.ClubModels
+{
+    public class Course
+    {
+
+        public string Code { get; set; }
+ 
+        public string year { get; set; }
+        public string Desription { get; set; }
+
+        public virtual ICollection<Student> CourseStudents { get; set; } 
+
+        
+    }
+}
---
+++ RADProjectWebAPI/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDomain.Classes.ClubModels
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public virtual ICollection<Course> StudentCourses { get; set; }
+    }
+}
