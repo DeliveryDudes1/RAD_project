62e951043b9b45b5146b1fbe3483d421c46a81d3
marek <marekherej@gmail.com>
2019-03-25 23:31:24 +0000

    Controllers in MVC Updated With HTTP response

--- RADPrpjectMVC/Controllers/StudentController.cs
+++ RADPrpjectMVC/Controllers/StudentController.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace RADPrpjectMVC.Controllers
 {
-    public class StudentController : ApiController
+    public class StudentController : Controller
     {
-        // GET: Student
-        public ActionResult Index()
+        // GET: Students
+        public async Task<ActionResult> Index()
         {
+            string apiUrl = "http://localhost:58764/api/values";
 
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+
+                }
+
+
+            }
             return View();
 
         }
