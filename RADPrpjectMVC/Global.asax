679e262b9aabcd90e72b63faa262449bf076ad65
marek <marekherej@gmail.com>
2019-03-25 17:08:07 +0000

    MVC project added

---
+++ RADPrpjectMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RADPrpjectMVC.MvcApplication" Language="C#" %>
---
+++ RADPrpjectMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RADPrpjectMVC.Startup))]
+namespace RADPrpjectMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
